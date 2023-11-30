c91deed88330caf41c3ee165e0893e443de0dee2
QHarris29 <grg290000000@gmail.com>
2023-11-29 21:02:16 -0800

    finished pt 2

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+// Background
+let horizon = window.innerHeight / 1.45
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+// Background
+
+// Inventory
+let inventory = document.createElement('div')
+// Inventory
+
+// ^^ Global Scope ^^ //
+
+function background(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -10,10 +32,15 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img');
+        inventoryItem.src = url;
+        inventory.append(inventoryItem);
+    })
 }
 
 function newInventory(){
-    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -26,10 +53,17 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
+// ^^ Functions ^^ //
+
+background('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+background('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
 newInventory()
-newImage('assets/green-character.gif', 100, 250)
+
+newImage('assets/green-character.gif', 100, 200)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
 newImage('assets/pine-tree.png', 450, 350)
@@ -39,3 +73,5 @@ newImage('assets/well.png', 500, 575)
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
+
+// ^^ Logs ^^ //
